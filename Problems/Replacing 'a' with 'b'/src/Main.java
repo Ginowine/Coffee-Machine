208a74162cded4be6e677f3d1df0c0d97b8d972b
ginowine <ginowine1@gmail.com>
2020-07-16 12:09:53 +0100

    string replace

---
+++ Problems/Replacing 'a' with 'b'/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.next();
+        String newWord = word.replace("a", "b");
+
+        System.out.println(newWord);
+    }
+}
