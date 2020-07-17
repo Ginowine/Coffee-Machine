03d0bc03f38307318e0b5b99c68d63cb5ea80e2a
ginowine <ginowine1@gmail.com>
2020-07-17 12:06:39 +0100

    using the AND operator

---
+++ Problems/Check a number is positive/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        if (number >= 1 ){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
