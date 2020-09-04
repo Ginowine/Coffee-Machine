17fbdc97d95faa9c842f90ab83af2b87e63a5fc3
ginowine <ginowine1@gmail.com>
2020-09-04 22:48:28 +0100

    Array declaration and initialization

--- Problems/Check prefix ignoring the case/src/Main.java
+++ Problems/Check prefix ignoring the case/src/Main.java
@@ -1,17 +1,5 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         // put your code here
-        Scanner scanner = new Scanner(System.in);
-        String word = scanner.nextLine();
-
-        String theFirstChar = word.substring(0).toUpperCase();
-
-        if (theFirstChar.equals("J")){
-            System.out.println(true);
-        }else {
-            System.out.println(false);
-        }
     }
 }
--- Problems/The integer barrier/src/Main.java
+++ Problems/The integer barrier/src/Main.java
@@ -1,22 +1,5 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         // put your code here
-
-        int sum = 0;
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-
-        while (scanner.hasNext()){
-            sum += number;
-            number = scanner.nextInt();
-            if (sum >= 1000){
-                sum -= 1000;
-            }
-
-        }
-        System.out.println(sum);
-
     }
 }
