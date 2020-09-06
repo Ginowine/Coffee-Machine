488ad682f93b92497b56e9d9d9981ba3f5b8bf0d
ginowine <ginowine1@gmail.com>
2020-09-07 00:53:41 +0100

    check if an array contains two numbers

---
+++ Problems/Check if an array contains two numbers/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        boolean var = false;
+
+        int sizeOfArray = scanner.nextInt();
+        int[] unsortedArray = new int[sizeOfArray];
+        for (int i = 0; i < sizeOfArray; i++){
+            unsortedArray[i] = scanner.nextInt();
+        }
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        for (int i = 0; i < unsortedArray.length -1; i++){
+            if (unsortedArray[i] == n && unsortedArray[i + 1] == m || unsortedArray[i] == m && unsortedArray[i + 1] == n){
+                var = true;
+                break;
+            }
+        }
+        if (var){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+
+    }
+}
