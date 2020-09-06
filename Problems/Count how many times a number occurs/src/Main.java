d8652221c7cff44f49adc882b8415a23aa3c70b5
ginowine <ginowine1@gmail.com>
2020-09-06 11:52:20 +0100

    for each loop to count how many times a number appeared in an array

--- Problems/Count how many times a number occurs/src/Main.java
+++ Problems/Count how many times a number occurs/src/Main.java
@@ -16,8 +16,14 @@ class Main {
         }
         n = scanner.nextInt();
 
-        for (int i = 0; i < numbers.length; i++){
-            if (numbers[i] == n){
+//        for (int i = 0; i < numbers.length; i++){
+//            if (numbers[i] == n){
+//                counter++;
+//            }
+//        }
+
+        for (int number : numbers){
+            if (number == n){
                 counter++;
             }
         }
