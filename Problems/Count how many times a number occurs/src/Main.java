2fd9f61480422fc9f190bbb5994960c931b3e26e
ginowine <ginowine1@gmail.com>
2020-09-06 11:43:46 +0100

    count how many times a number occurred in an array

---
+++ Problems/Count how many times a number occurs/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 0;
+        int counter = 0;
+
+        int sizeOfArray = scanner.nextInt();
+        int[] numbers = new int[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++){
+            numbers[i] = scanner.nextInt();
+        }
+        n = scanner.nextInt();
+
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] == n){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+}
--- Problems/Digits from 0 to 9 in a single line/src/Main.java
+++ Problems/Digits from 0 to 9 in a single line/src/Main.java
@@ -1,5 +1,14 @@
 class Main {
     public static void main(String[] args) {
         // put your code here
+
+        int [] numbers = {1, 2, 3, 4};
+        //int sum = 0;
+
+        for (int i = numbers.length -1; i >= 0; i--){
+            System.out.println(numbers[i]);
+        }
+
+
     }
 }
