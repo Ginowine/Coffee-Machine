ba9bf067ad4fd1ae5b030cc19efb5e0a49d87920
ginowine <ginowine1@gmail.com>
2020-09-07 14:41:47 +0100

    longest ascending sequence of an array

--- Problems/The longest ascending sequence/src/Main.java
+++ Problems/The longest ascending sequence/src/Main.java
@@ -1,38 +1,26 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // put your code here
 
         Scanner scanner = new Scanner(System.in);
-        int counter = 0;
-        int answer = 0;
+        int n = scanner.nextInt();
 
-        int size = scanner.nextInt();
-        int[] input = new int[size];
-
-        for (int i = 0; i < size; i++){
-            input[i] = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-        Arrays.sort(input);
-
-        int longestStreak = 1;
-        int currentStreak = 1;
-
-        for (int i = 1; i < input.length; i++){
-            if (input[i] != input[i -1]){
-                if (input[i] == input[i -1] +1){
-                    currentStreak +=1;
-                }else {
-                    longestStreak = Math.max(longestStreak, currentStreak);
-                    currentStreak = 1;
-                }
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            int best = 1;
+            while (i < n && arr[i] > arr[i - 1]) {
+                best++;
+                i++;
             }
+            max = Math.max(max, best);
         }
 
-        System.out.println(Math.max(longestStreak, currentStreak));
-
+        System.out.println(max);
     }
 }
