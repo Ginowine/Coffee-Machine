c59c7bf541d1ef13714deba3e78eda25a1d06f49
ginowine <ginowine1@gmail.com>
2020-09-10 11:49:27 +0100

    2 dimensional array

--- Problems/The star figure/src/Main.java
+++ Problems/The star figure/src/Main.java
@@ -4,18 +4,32 @@ class Main {
     public static void main(String[] args) {
         // put your code here
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
+        String[][] arr = new String[n][n];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (j == n / 2 || i == n / 2 || j == i || j == n - (i + 1)) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(".");
-                }
-                System.out.print(" ");
+                arr[i][j] = fill(i, j, n);
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
+
+    }
+
+    static String fill(int i, int j, int n) {
+        if (i == j) { // main diagonal
+            return "*";
+        }
+        if (i == n - j - 1) { // secondary diagonal
+            return "*";
+        }
+        if (i == n / 2) { // middle row
+            return "*";
+        }
+        if (j == n / 2) { // middle column
+            return "*";
+        }
+        return ".";
     }
 }
